dfe0a08d2b76374e911d45e63efe6028f92332a9
Josh Gadeken <josh@steelcaverobotics.com>
2019-01-23 22:39:36 -0700

    Separate libeuler test data JSON object members by newlines

--- libeuler_test_data.py
+++ libeuler_test_data.py
@@ -10,17 +10,23 @@ DEFAULT_FILENAME = "libeuler_test_data.json"
 def load(filename=DEFAULT_FILENAME) -> dict:
     """Return decoded JSON from filename"""
     with open(filename, "r") as inf:
-        return json.loads(inf.read())
+        return json.load(inf)
 
 
 def save(test_data, filename=DEFAULT_FILENAME):
     """Encode test_data as JSON and write to filename"""
     with open(filename, "w") as outf:
-        outf.write(json.dumps(test_data, sort_keys=True))
+        # write each json object member on its own line
+        outf.write("{\n")
+        json_items = []
+        for k, v in sorted(test_data.items()):
+            json_str = json.dumps(v, sort_keys=True)
+            json_items.append("\"{}\": {}".format(k, json_str))
+        outf.write(",\n".join(json_items))
+        outf.write("\n}\n")
 
 
 if __name__ == "__main__":
-    #pylint: disable=invalid-name
     data = load()
     print("JSON data loaded into variable 'data'")
     print("data.keys() = {}\n".format(sorted(data.keys())))
